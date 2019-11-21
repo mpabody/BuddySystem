5bc99d120ce43ea3d949c03f91bc701f96b29823
Michael Pabody <michael.pabody@gmail.com>
2019-11-20 22:10:08 -0500

    changed max character length of Buddy Name in Create and Edit models to 40

--- BuddySystem.Models/BuddyModels/BuddyCreate.cs
+++ BuddySystem.Models/BuddyModels/BuddyCreate.cs
@@ -11,12 +11,12 @@ namespace BuddySystem.Models
     {
         [Required]
         [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
-        [MaxLength(20, ErrorMessage = "There are too many characters in this field.")]
+        [MaxLength(40, ErrorMessage = "There are too many characters in this field.")]
         public string Name { get; set; }
 
         [Required]
         [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
-        [MaxLength(20, ErrorMessage = "There are too many characters in this field.")]
+        [MaxLength(40, ErrorMessage = "There are too many characters in this field.")]
         public string CurrentLocation { get; set; }
 
         [Required]
