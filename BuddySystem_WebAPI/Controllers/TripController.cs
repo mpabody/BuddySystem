6c11c023bb348d2469becf73c5b3b0946327ef4e
Michael Pabody <michael.pabody@gmail.com>
2019-11-22 17:01:31 -0500

    started to work on Trip Controller. Committing so I can pull Casey's changes to the services files

---
+++ BuddySystem_WebAPI/Controllers/TripController.cs
@@ -0,0 +1,45 @@
+﻿using BuddySystem.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BuddySystem_WebAPI.Controllers
+{
+    [Authorize]
+    public class TripController : ApiController
+    {
+        public IHttpActionResult GetTripsForUser()
+        {
+            TripService tripService = CreateTripService();
+            var trips = tripService.GetTripsForCurrentUser();
+            return Ok(trips);
+        }
+
+
+
+
+
+        private TripService CreateTripService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new TripService(userId);
+            return service;
+        }
+        private BuddyService CreateBuddyServiceNoGuid()
+        {
+            var buddyServiceNoGuid = new BuddyService();
+            return buddyServiceNoGuid;
+        }
+        private AdditionalBuddyService CreateAdditionalBuddyService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var additionalBuddyService = new AdditionalBuddyService(userId);
+            return additionalBuddyService;
+        }
+
+    }
+}
