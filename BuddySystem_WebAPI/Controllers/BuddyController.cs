2f363a8597b4c2f4ff1b8636b8092c5fe863cd01
Michael Pabody <michael.pabody@gmail.com>
2019-11-21 19:12:44 -0500

    added buddy controller. all methods working when tested in postman

---
+++ BuddySystem_WebAPI/Controllers/BuddyController.cs
@@ -0,0 +1,77 @@
+﻿using BuddySystem.Models;
+using BuddySystem.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BuddySystem_WebAPI.Controllers
+{
+    [Authorize]
+    public class BuddyController : ApiController
+    {
+
+        public IHttpActionResult GetAllBuddies()
+        {
+            BuddyService buddyService = CreateBuddyService();
+            var buddies = buddyService.GetAllBuddies();
+            return Ok(buddies);
+        }
+
+        public IHttpActionResult GetBuddy(int id)
+        {
+            BuddyService buddyService = CreateBuddyService();
+            var buddy = buddyService.GetBuddyById(id);
+            return Ok(buddy);
+        }
+
+        // Create
+        public IHttpActionResult Post(BuddyCreate buddy)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateBuddyService();
+
+            if (!service.CreateBuddy(buddy))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        // Update
+        public IHttpActionResult Put(BuddyEdit buddy)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreateBuddyService();
+
+            if (!service.UpdateBuddy(buddy))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var service = CreateBuddyService();
+
+            if (!service.DeleteBuddy(id))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+
+        private BuddyService CreateBuddyService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new BuddyService(userId);
+            return service;
+        }
+    }
+}
