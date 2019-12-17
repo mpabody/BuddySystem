8520efc492b702102b47678e72b71a5c8b8e79e3
Michael Pabody <michael.pabody@gmail.com>
2019-12-17 15:37:54 -0500

    fixed casey's email stuff

--- BuddySystem_WebAPI/Controllers/AccountController.cs
+++ BuddySystem_WebAPI/Controllers/AccountController.cs
@@ -331,7 +331,7 @@ namespace BuddySystem_WebAPI.Controllers
                 return BadRequest(ModelState);
             }
 
-            var user = new ApplicationUser() { UserName = model.Email,  }; //Email = model.Email
+            var user = new ApplicationUser() { UserName = model.Email, Email = model.Email };
 
             IdentityResult result = await UserManager.CreateAsync(user, model.Password);
 
